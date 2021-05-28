3b5dde13ad5e1c6e1a626e55145f76115028ffc2
pororo0721 <  >
2021-05-28 14:57:19 +0100

    Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,11 +34,19 @@ export const getEdit = async(req, res) => {
   return res.render("edit", { pageTitle: `Edit:${video.title}`, video});
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async(req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title,description,hashtags } = req.body;
+  const video = await Video.exists({_id: id});
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id,{
+    title,description, hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
+  
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
@@ -50,7 +58,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
@@ -60,3 +68,10 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo = async (req,res) =>{
+  const {id} = req.params;
+  await Video.findByIdAndDelete(id);
+
+  return res.redirect("/");
+}
