dcb18107ec0c71753a9629844d9133ba7e1d7edf
pororo0721 <  >
2021-05-29 09:57:16 +0100

    search and create account

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,7 @@ console.log("finished")
 */
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({createdAt:"desc"});
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -75,3 +75,17 @@ export const deleteVideo = async (req,res) =>{
 
   return res.redirect("/");
 }
+
+export const search = async(req, res) => {
+  const {keyword} = req.query;
+  let videos =[];
+  if(keyword){
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+    console.log(videos);
+  }
+  return res.render("search",{ pageTitle: "Search",videos });
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -14,11 +11,8 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
