51281be061727cb87b53869062b39310e77ab0e7
pororo0721 <  >
2021-06-01 16:34:01 +0100

    Logged In User

---
+++ src/middlewares.js
@@ -0,0 +1,9 @@
+export const localsMiddleware = (req, res, next) =>{
+    
+    if(req.session.loggedIn){
+        res.locals.loggedIn = true
+    }
+    res.locals.siteName ="Wetube";
+    res.locals.loggedInUser = req.session.user;
+    next();
+};
