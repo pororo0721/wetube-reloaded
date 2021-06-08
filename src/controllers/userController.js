778e547b9e5f127ee9c13103a2b4fc370828fbdf
pororo0721 <  >
2021-06-08 14:02:14 +0100

    Edit-profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,55 +3,56 @@ import fetch from "node-fetch";
 import bcryptjs from "bcryptjs";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = async(req, res) => {
-  const {name, username, email, password, password2, location} = req.body;
-  const pageTitle ="Join";
-  if(password !== password2){
-    return res.status(400).render("join", { 
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
   }
-  const exists = await User.exists({ $or: [{username}, {email} ]});
-  if(exists) {
-    return res.status(400).render("join", { 
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already taken.",
-   });
-  }
-try{
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
-} catch (error) {
-  return res.status(400).render("join", {
-    pageTitle: "Upload Video",
-    errorMessage: error._message,
-  });
-}
-};
-export const getLogin = (req, res) => res.render("login",{pageTitle: "Login"});
-
-export const postLogin = async(req,res) =>{
-  const {username, password} = req.body;
-  const pageTitle = "Login";
-  const user = await User.findOne({username, socialOnly: false});
-  if(!user) {
-    return res.status(400).render("login",{
-      pageTitle, 
-      errorMessage:"An account with this username does not exists."
     });
   }
-  const ok = await bcryptjs.compare(password, user.password);
-  if(!ok){
-    return res.status(400).render("login",{
-      pageTitle, 
-      errorMessage:"Wrong password",
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username, socialOnly: false });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
     });
   }
   req.session.loggedIn = true;
@@ -90,7 +91,7 @@ export const finishGithubLogin = async (req, res) => {
   ).json();
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
-    const apiUrl ="https://api.github.com"
+    const apiUrl = "https://api.github.com";
     const userData = await (
       await fetch(`${apiUrl}/user`, {
         headers: {
@@ -98,36 +99,35 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    
     const emailData = await (
-        await fetch(`${apiUrl}/user/emails`, {
-          headers: {
-            Authorization: `token ${access_token}`,
-          },
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
       })
     ).json();
     const emailObj = emailData.find(
-      (email) => email.primary === true && email.verified ===true
+      (email) => email.primary === true && email.verified === true
     );
-    if(!emailObj) {
+    if (!emailObj) {
+      // set notification
       return res.redirect("/login");
     }
-    let user = await User.findOne({email: emailObj.email});
-    if(!user) {
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
       user = await User.create({
-          avatarUrl:userData.avatar_url,
-          name: userData.name,
-          username:userData.login,
-          email: emailObj.email,
-          password:"",
-          socialOnly: true,
-          location:userData.location,
+        avatarUrl: userData.avatar_url,
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
       });
-    }  
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
-  
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
@@ -135,15 +135,41 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
-  res.redirect("/");
+  return res.redirect("/");
 };
-
-export const getEdit =(req, res) =>{
-  return res.render("edit-profile",{pageTitle:"Edit Profile"});
-}
-
-export const postEdit =(req,res)=>{
-  return res.render("edit-profile");
-}
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  
+  const pageTitle ="Edit Profile";
+  const exixts = await User.exists({
+    $and:[{_id:{$ne:_id}},{$or: [{username},{email}] }],
+  });
+  if(exixts) {
+    return res.status(400).render("edit-profile",{
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.render("edit-profile",{pageTitle});
+};
 
 export const see = (req, res) => res.send("See User");
