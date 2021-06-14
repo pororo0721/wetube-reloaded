580cadba15adafa0fa9d8a5651066d3e71b7490f
pororo0721 <  >
2021-06-14 15:43:00 +0100

    Styles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,4 @@
 import User from "../models/User";
-import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcryptjs from "bcryptjs";
 
@@ -210,7 +209,14 @@ export const postChangePassword = async(req, res) =>{
 
 export const see = async (req, res) => {
   const {id}= req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
+
   if(!user){
     return res.status(404).render("404", {pageTitle: "User not found."});
   }
