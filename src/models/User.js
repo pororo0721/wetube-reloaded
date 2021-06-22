3db0d5e91749086d8da74f2df041717209686320
pororo0721 <  >
2021-06-22 13:44:01 +0100

    Comment Models

--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   location: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   videos: [{type: mongoose.Schema.Types.ObjectId, ref: "Video"},]
 });
 
