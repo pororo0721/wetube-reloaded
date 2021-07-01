4e62e58ef084aaf49655ab8e3c5db46027a76071
pororo0721 <  >
2021-07-01 08:42:19 +0100

    AWS S3

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials:{
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+  }
+})
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: 'nyangtube'
+})
 
 export const localsMiddleware = (req, res, next) =>{
     
@@ -33,11 +47,13 @@ export const protectorMiddleware = (req, res, next) => {
     limits:{
       fileSize: 30000000,
   },
+  storage: multerUploader,
 });
+
   export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 100000000,
   },
-
+  storage: multerUploader,
 });
